bd72a4864449c58f807acd535e0e1ea62f9bef69
Chinar <huabnjs@163.com>
2018-11-14 17:50:16 +0800

    首次提交教程项目
    学习下UniRx，顺便将所学做记录，以便后期查看，供他人了解！

---
+++ Assets/ChinarDemo/Editor/ChinarScriptFirstComment.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace UGUIFrameWorkEditor
+{
+    public class ChinarScriptFirstComment : UnityEditor.AssetModificationProcessor
+    {
+        /// <summary>
+        /// 在资源创建时调用
+        /// </summary>
+        /// <param name="path">自动传入资源路径</param>
+        public static void OnWillCreateAsset(string path)
+        {
+            path = path.Replace(".meta", "");
+            if (!path.EndsWith(".cs")) return;
+            string allText = "// ========================================================\r\n"
+                             + "// 描述：\r\n"
+                             + "// 作者：Chinar \r\n"
+                             + "// 创建时间：#CreateTime#\r\n"
+                             + "// 版 本：1.0\r\n"
+                             + "// ========================================================\r\n";
+            allText += File.ReadAllText(path);
+            allText =  allText.Replace("#CreateTime#", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            File.WriteAllText(path, allText);
+        }
+    }
+}
